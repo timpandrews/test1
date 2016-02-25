17b26aaa02abcaef610ee4f951fa85c0876f7545
Tim Andrews <tim.andrews@imagineifinc.com>
2016-02-25 14:39:33 -0500

    Basic Bootstrap Template

--- test1/settings.py
+++ test1/settings.py
@@ -126,5 +126,6 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 
+# Media Files (Files uploaded through application ie profile pics)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
--- test1/urls.py
+++ test1/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import  static
 from django.contrib import admin
+
 import app1.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', app1.views.home, name='home'),
     url(r'^home/', app1.views.home, name='home'),
-
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
