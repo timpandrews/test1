d120dd538b234b2cc6ba619b3e587f9ad80d0553
Tim Andrews <tim.andrews@imagineifinc.com>
2016-02-25 20:31:03 -0500

    User Model

--- app1/admin.py
+++ app1/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "timestamp", "updated"]
+    class Meta:
+        model = User
+
+admin.site.register(User, UserAdmin)
--- app1/models.py
+++ app1/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(blank=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __unicode__(self):
+        return self.username
+
