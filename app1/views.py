5da7d150f451aec8a591d39b89f019e7b809f461
Tim Andrews <tim.andrews@imagineifinc.com>
2016-02-27 15:34:36 -0500

    registration form + requirements.txt

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .forms import User_Registartion_Form
+
 # Create your views here.
 def home(request):
 
@@ -13,8 +15,23 @@ def home(request):
 def registration(request):
 
     msg = "Welcome"
+    show_form = True
+
+    form = User_Registartion_Form(request.POST or None)
+
     context = {
         "msg": msg,
+        "show_form": show_form,
+        "form": form,
     }
+
+    if form.is_valid():
+        form.save()
+        msg = "Thank you for registering!"
+        show_form = False
+        context = {
+            "msg": msg,
+            "show_form": show_form,
+        }
 
     return render(request, "registration.html", context)
